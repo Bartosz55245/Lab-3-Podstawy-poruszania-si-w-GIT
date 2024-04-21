fdaafd3797c01d4e4cbad640a2054220198adc77
Bartosz55245 <dsw55245@student.dsw.edu.pl>
2024-04-21 16:59:41 +0200

    komendy

---
+++ komendy.sh
@@ -0,0 +1,23 @@
+git commit
+git branch
+git checkout 
+git merge
+git branch -f
+git cherry-pick
+git reset
+git revert
+git rebase
+git rebase -i
+git commit --amend
+git tag
+git describe
+git clone
+git fetch
+git pull
+git push
+git pull --rebase
+git checkout -b
+git branch -u 
+git push origin :foo
+git fetch origin :bar
+git pull origin main:foo
